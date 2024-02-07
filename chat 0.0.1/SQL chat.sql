4c2497ac6477dec8d27915e9741ac6f1af1b24b2
ImParks <152468535+ImParks@users.noreply.github.com>
2024-02-07 10:48:28 +0900

    Add files via upload

---
+++ chat 0.0.1/SQL chat.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE chat DEFAULT CHARACTER SET UTF8MB4;
+use chat;
+
+CREATE TABLE login(
+no int primary key auto_increment, # 유저 고유 넘버
+id char(30) unique, # 유저 아이디
+pw char(30) , # 유저 비밀번호
+name char(30), # 유저 닉네임
+name_num int default 1, # 유저 태그번호(중복일떄 1씩 증가
+createdAt datetime not null default now(),
+updatedAt datetime not null default now() on update now()
+);
+drop table login;
+SELECT * FROM login ;
+SELECT COUNT(*) login WHERE id = '1';
+SELECT * FROM login WHERE name like '';
+
+CREATE TABLE chatroom( 
+no int primary key auto_increment, # 채팅방 고유번호
+cat_name char(50), # 채팅방 이름
+open int default 0, # 0일때 오픈방 1일때 닫힌방
+member int default 10, # 맴버 최대 인원수
+createdAt datetime not null default now(),
+updatedAt datetime not null default now() on update now()
+);
+
+CREATE TABLE chatjoin( # 아이디 랑 채팅방이랑 링크하는 테이블
+id_no int, 
+room_no int,
+createdAt datetime not null default now(),
+updatedAt datetime not null default now() on update now()
+);
+
+
+select * from chatroom where login_num = no
+
+CREATE TABLE chatting( # 이거는 계속 늘어나게 해야함. 채팅방 채팅로그
+#미리 만들어두지 말고 자바 안에서 생성 삭제를 하는방식으로
+# 채팅방 고유번호, 위에 챗룸과 동일해야함
+# 채팅친사람 고유번호 --> 채팅 삭제 및 수정을 위해 필요
+# 채팅친 사람의 닉네임
+# 채팅
+# 시간 <-- 한달이 지나면 자동으로 글이 삭제 되게 만듦
+ 
+);
+
